6bfe2f958383c7375012ac9dbed3ccf50c1f960d
Lewis Cianci <lewis@MacBook-Pro.local>
2021-12-21 07:13:38 +1000

    initial

---
+++ 2 - sitewide-theme/theme.js
@@ -0,0 +1,65 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const toggleButton = document.querySelector('.toggle-button');
+    toggleButton.addEventListener('change', toggleTheme, false);
+
+    const preferredTheme = localStorage.getItem('theme');
+    if (preferredTheme === 'dark') {
+        useTheme('dark');
+        document.querySelector('.toggle-button').checked = true;
+    } else {
+        useTheme('light');
+        document.querySelector('.toggle-button').checked = false;
+    };
+
+})
+
+const theme = {
+    dark: {
+        '--primary-color': '#325b97',
+        '--secondary-color': '#9cafeb',
+        '--font-color': '#e1e1ff',
+        '--bg-color': '#000013',
+        '--heading-color': '#818cab'
+    },
+    light: {
+        '--primary-color': '#0d0b52',
+        '--secondary-color': '#3458b9',
+        '--font-color': '#424242',
+        '--bg-color': '#ffffff',
+        '--heading-color': '#292922'
+    }
+};
+
+function toggleTheme(e) {
+    if (e.target.checked) {
+        useTheme('dark');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        useTheme('light');
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+function useTheme(themeChoice) {
+    document.documentElement.style.setProperty(
+        '--primary-color',
+        theme[themeChoice]['--primary-color']
+    );
+    document.documentElement.style.setProperty(
+        '--secondary-color',
+        theme[themeChoice]['--secondary-color']
+    );
+    document.documentElement.style.setProperty(
+        '--font-color',
+        theme[themeChoice]['--font-color']
+    );
+    document.documentElement.style.setProperty(
+        '--bg-color',
+        theme[themeChoice]['--bg-color']
+    );
+    document.documentElement.style.setProperty(
+        '--heading-color',
+        theme[themeChoice]['--heading-color']
+    );
+}
+
